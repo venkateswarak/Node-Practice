25df32ae8f2fcfec098bae63c2b1b55225f9806d
venkateswarak <kothav4@gmail.com>
2017-04-20 11:16:18 -0500

    assignment 4/20
    Arthmetic Operations

---
+++ arthopr.js
@@ -0,0 +1,9 @@
+var sum=require('./arthmetic');
+var mul=require('./arthmetic');
+var sub=require('./arthmetic');
+var div=require('./arthmetic');
+
+console.log(sum.add(14,15));
+console.log(mul.mul(14,15));
+console.log(sub.sub(14,15));
+console.log(div.div(14,15));
